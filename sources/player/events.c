3e30ff115f71498e84927e0dbe1d6a57ba76803c
thlibers <thlibers@student.42lehavre.fr>
2026-04-08 16:12:14 +0200

    add player movements

--- sources/player/events.c
+++ sources/player/events.c
@@ -6,7 +6,7 @@
 /*   By: thlibers <thlibers@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 09:59:26 by thlibers          #+#    #+#             */
-/*   Updated: 2026/04/08 15:52:28 by thlibers         ###   ########.fr       */
+/*   Updated: 2026/04/08 16:11:05 by thlibers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ int	handle_keypress(int keycode, t_game *game)
 	printf("keycode : %d\n", keycode);
 	if (keycode == KEY_ESC)
 		handle_close(game);
-	if (keycode == KEY_W)
+	if (keycode == KEY_W && !(keycode == KEY_S))
 		move_player(game, game->player.dir_x * PLA_SPEED, game->player.dir_y * PLA_SPEED);		// les controles ne suivent pas le joueur
-	if (keycode == KEY_S)
+	if (keycode == KEY_S && !(keycode == KEY_W))
 		move_player(game, -game->player.dir_x * PLA_SPEED, -game->player.dir_y * PLA_SPEED);		// quand deux input sont presses simutanement
-	if (keycode == KEY_D)																				// le joueur bouge quand meme
-		move_player(game, game->player.dir_x * PLA_SPEED, -game->player.dir_y * PLA_SPEED);
-	if (keycode == KEY_A)
-		move_player(game, -game->player.dir_x * PLA_SPEED, game->player.dir_y * PLA_SPEED);
-	if (keycode == KEY_RIGHT)
+	if (keycode == KEY_D && !(keycode == KEY_A))																				// le joueur bouge quand meme
+		move_player(game, -game->player.dir_y * PLA_SPEED, game->player.dir_x * PLA_SPEED);
+	if (keycode == KEY_A && !(keycode == KEY_D))
+		move_player(game, game->player.dir_y * PLA_SPEED, -game->player.dir_x * PLA_SPEED);
+	if (keycode == KEY_RIGHT && !(keycode == KEY_LEFT))
 		move_camera(game, ROT_SPEED);
-	if (keycode == KEY_LEFT)
+	if (keycode == KEY_LEFT && !(keycode == KEY_RIGHT))
 		move_camera(game, -ROT_SPEED);
 	return (0);
 }
